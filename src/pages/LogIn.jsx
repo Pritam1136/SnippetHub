f414963dc98ab3b723e6b76714ef75c5f6a0277b
Pritam Roy <pritamroy1136@gmail.com>
2023-05-25 23:32:23 +0530

    Updated Login Page

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,124 +1,130 @@
 export const LogIn = () => {
   return (
-    <section className="body-font relative h-screen bg-gray-900 text-gray-400">
-      <div className="container mx-auto px-5 py-24">
-        <div className="mb-12 flex w-full flex-col text-center">
-          <h1 className="title-font mb-4 text-2xl font-medium text-white sm:text-3xl">
-            LogIn
-          </h1>
-          <p className="mx-auto text-base leading-relaxed lg:w-2/3">
-            LogIn with email and password.
-          </p>
-        </div>
-        <div className="mx-auto md:w-2/3 lg:w-1/2">
-          <div className="-m-2 flex flex-wrap">
-            <div className="w-1/2 p-2">
-              <div className="relative">
-                <label
-                  htmlFor="email"
-                  className="text-sm leading-7 text-gray-400"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full rounded border border-gray-700 bg-gray-800 bg-opacity-40 px-3 py-1 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900"
-                />
+    <div>
+      <div className="h-px w-auto bg-slate-500"></div>
+      <section className="body-font relative  bg-gray-900 text-gray-400">
+        <div className="container mx-auto w-fit px-5 py-24">
+          <div className="mb-12 flex  flex-col text-center">
+            <h1 className="title-font mb-4 text-2xl font-medium text-white sm:text-3xl">
+              LogIn
+            </h1>
+            <p className="mx-auto text-base leading-relaxed lg:w-2/3">
+              LogIn with email and password.
+            </p>
+          </div>
+          <div className="mx-auto md:w-2/3 lg:w-1/2">
+            <div className="-m-2 flex flex-wrap">
+              <div className="w-1/2 p-2">
+                <div className="relative">
+                  <label
+                    htmlFor="email"
+                    className="text-sm leading-7 text-gray-400"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full rounded border border-gray-700 bg-gray-800 bg-opacity-40 px-3 py-1 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900"
+                  />
+                </div>
               </div>
-            </div>
-            <div className="w-1/2 p-2">
-              <div className="relative">
-                <label
-                  htmlFor="password"
-                  className="text-sm leading-7 text-gray-400"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  className="w-full rounded border border-gray-700 bg-gray-800 bg-opacity-40 px-3 py-1 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900"
-                />
+              <div className="w-1/2 p-2">
+                <div className="relative">
+                  <label
+                    htmlFor="password"
+                    className="text-sm leading-7 text-gray-400"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="w-full rounded border border-gray-700 bg-gray-800 bg-opacity-40 px-3 py-1 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900"
+                  />
+                </div>
               </div>
-            </div>
-            <div className="mt-4 w-full p-2">
-              <button className="mx-auto flex rounded border-0 bg-indigo-500 px-8 py-2 text-lg text-white hover:bg-indigo-600 focus:outline-none">
-                LogIn
-              </button>
-            </div>
-            <div className="mt-12 w-full border-t border-gray-800 p-2 pt-8 text-center">
-              <p>Contact us on:</p>
-              <a className="text-indigo-400">snippethub01@email.com</a>
-              <p className="my-5 mt-5 leading-normal">
-                49 Smith St.
-                <p>Saint Cloud, MN 56301</p>
-              </p>
-              <span className="inline-flex">
-                <a className="text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-5 w-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
-                </a>
-                <a className="ml-4 text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-5 w-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
-                </a>
-                <a className="ml-4 text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-5 w-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <rect
-                      width="20"
-                      height="20"
-                      x="2"
-                      y="2"
-                      rx="5"
-                      ry="5"
-                    ></rect>
-                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                  </svg>
-                </a>
-                <a className="ml-4 text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-5 w-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                  </svg>
-                </a>
-              </span>
+              <div className="mt-4 w-full p-2">
+                <button className="mx-auto flex rounded border-0 bg-indigo-500 px-8 py-2 text-lg text-white hover:bg-indigo-600 focus:outline-none">
+                  LogIn
+                </button>
+              </div>
+              <div className="mx-auto mt-10 font-extrabold text-white">
+                <button className="border-spacing-2">Login with Google</button>
+              </div>
+              <div className="mt-12 w-full border-t border-gray-800 p-2 pt-8 text-center">
+                <p>Contact us on:</p>
+                <a className="text-indigo-400">snippethub01@email.com</a>
+                <div className="my-5 mt-5 leading-normal">
+                  49 Smith St.
+                  <p>Saint Cloud, MN 56301</p>
+                </div>
+                <span className="inline-flex">
+                  <a className="text-gray-500">
+                    <svg
+                      fill="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="h-5 w-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    </svg>
+                  </a>
+                  <a className="ml-4 text-gray-500">
+                    <svg
+                      fill="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="h-5 w-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                    </svg>
+                  </a>
+                  <a className="ml-4 text-gray-500">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="h-5 w-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <rect
+                        width="20"
+                        height="20"
+                        x="2"
+                        y="2"
+                        rx="5"
+                        ry="5"
+                      ></rect>
+                      <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                    </svg>
+                  </a>
+                  <a className="ml-4 text-gray-500">
+                    <svg
+                      fill="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="h-5 w-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                    </svg>
+                  </a>
+                </span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
