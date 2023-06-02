bb1093c5fe9a99b31458b056b44dbcfb5cc3f2c7
Pritam Roy <pritamroy1136@gmail.com>
2023-06-02 13:15:44 +0530

    bug fixed

--- src/modules/modulesNavigate/ModulesNav.jsx
+++ src/modules/modulesNavigate/ModulesNav.jsx
@@ -12,89 +12,90 @@ export const ModulesNav = () => {
           <div className="text-center">
             <Link to={"/modules/buttons"}>
               <button>
-                <a
+                <span
                   href="#_"
                   className="group relative rounded-lg px-6 py-3 font-bold text-white"
                 >
                   <span className="ease absolute inset-0 h-full w-full -translate-x-1 -translate-y-1 transform bg-purple-800 opacity-80 transition duration-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
                   <span className="ease absolute inset-0 h-full w-full translate-x-1 translate-y-1 transform bg-pink-800 opacity-80 mix-blend-screen transition duration-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
                   <span className="relative">BUTTONS</span>
-                </a>
+                </span>
               </button>
             </Link>
           </div>
           <div className="mt-16 text-center">
             <Link to={"/modules/Cards"}>
               <button>
-                <a
+                <span
                   href="#_"
                   className="group relative rounded-lg px-6 py-3 font-bold text-white"
                 >
                   <span className="ease absolute inset-0 h-full w-full -translate-x-1 -translate-y-1 transform bg-purple-800 opacity-80 transition duration-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
                   <span className="ease absolute inset-0 h-full w-full translate-x-1 translate-y-1 transform bg-pink-800 opacity-80 mix-blend-screen transition duration-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
                   <span className="relative">CARDS</span>
-                </a>
+                </span>
               </button>
             </Link>
           </div>
           <div className="mt-16 text-center">
             <Link to={"/modules/headers"}>
               <button>
-                <a
+                <span
                   href="#_"
                   className="group relative rounded-lg px-6 py-3 font-bold text-white"
                 >
                   <span className="ease absolute inset-0 h-full w-full -translate-x-1 -translate-y-1 transform bg-purple-800 opacity-80 transition duration-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
                   <span className="ease absolute inset-0 h-full w-full translate-x-1 translate-y-1 transform bg-pink-800 opacity-80 mix-blend-screen transition duration-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
                   <span className="relative">HEADERS</span>
-                </a>
+                </span>
               </button>
             </Link>
           </div>
           <div className="mt-16 text-center">
             <Link to={"/modules/footers"}>
               <button>
-                <a
+                <span
                   href="#_"
                   className="group relative rounded-lg px-6 py-3 font-bold text-white"
                 >
                   <span className="ease absolute inset-0 h-full w-full -translate-x-1 -translate-y-1 transform bg-purple-800 opacity-80 transition duration-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
                   <span className="ease absolute inset-0 h-full w-full translate-x-1 translate-y-1 transform bg-pink-800 opacity-80 mix-blend-screen transition duration-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
                   <span className="relative">FOOTERS</span>
-                </a>
+                </span>
               </button>
             </Link>
           </div>
           <div className="mt-16 text-center">
             <Link to={"/modules/testimonials"}>
               <button>
-                <a
+                <span
                   href="#_"
                   className="group relative rounded-lg px-6 py-3 font-bold text-white"
                 >
                   <span className="ease absolute inset-0 h-full w-full -translate-x-1 -translate-y-1 transform bg-purple-800 opacity-80 transition duration-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
                   <span className="ease absolute inset-0 h-full w-full translate-x-1 translate-y-1 transform bg-pink-800 opacity-80 mix-blend-screen transition duration-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
                   <span className="relative">TESTIMONIALS</span>
-                </a>
+                </span>
               </button>
             </Link>
           </div>
-          <div className="mt-16 text-center">
+          <div className="mt-16 bg-gray-900 text-center">
             <Link to={"/modules/content"}>
               <button>
-                <a
+                <span
                   href="#_"
                   className="group relative rounded-lg px-6 py-3 font-bold text-white"
                 >
                   <span className="ease absolute inset-0 h-full w-full -translate-x-1 -translate-y-1 transform bg-purple-800 opacity-80 transition duration-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
                   <span className="ease absolute inset-0 h-full w-full translate-x-1 translate-y-1 transform bg-pink-800 opacity-80 mix-blend-screen transition duration-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
                   <span className="relative">CONTENTS</span>
-                </a>
+                </span>
               </button>
             </Link>
           </div>
         </div>
       </div>
+      <div className="h-20 bg-gray-900"></div>
     </div>
   );
 };
