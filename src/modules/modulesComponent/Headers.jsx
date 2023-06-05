4968e71267f5ab5508239925f4b6276915371ddc
Pritam Roy <pritamroy1136@gmail.com>
2023-06-05 16:59:47 +0530

    Added more headers

--- src/modules/modulesComponent/Headers.jsx
+++ src/modules/modulesComponent/Headers.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { useState } from "react";
 
 export const Headers = () => {
   return (
-    <div className="h-screen bg-slate-900">
+    <div className="min-h-screen items-center justify-center bg-slate-900">
       <div className="pt-8">
-        <header className="mx-4 bg-blue-500">
+        <header className=" bg-blue-500">
           <div className="container mx-auto flex items-center justify-between px-8 py-5">
-            <h1 className="text-xl font-bold text-white">
-              My Responsive Header
-            </h1>
+            <h1 className="text-xl font-bold text-white">Header</h1>
             <nav className="space-x-4">
               <a
                 href="#"
@@ -35,11 +32,9 @@ export const Headers = () => {
       </div>
 
       <div className="pt-8">
-        <header className="mx-4 bg-gradient-to-r from-purple-600 via-pink-500 to-red-500">
+        <header className=" bg-gradient-to-r from-purple-600 via-pink-500 to-red-500">
           <div className="container mx-auto flex items-center justify-between px-8 py-4">
-            <h1 className="text-3xl font-bold text-white">
-              My Responsive Header
-            </h1>
+            <h1 className="text-3xl font-bold text-white">Header</h1>
             <nav className="space-x-6">
               <a
                 href="#"
@@ -70,11 +65,9 @@ export const Headers = () => {
         </header>
       </div>
       <div className="pt-8">
-        <header className="mx-4 bg-gradient-to-r from-green-400 via-yellow-500 to-red-500">
+        <header className=" bg-gradient-to-r from-green-400 via-yellow-500 to-red-500">
           <div className="container mx-auto flex items-center justify-between px-8 py-4">
-            <h1 className="text-3xl font-bold text-white">
-              My Responsive Header
-            </h1>
+            <h1 className="text-3xl font-bold text-white">Header</h1>
             <nav className="space-x-6">
               <a
                 href="#"
@@ -97,6 +90,72 @@ export const Headers = () => {
               <a
                 href="#"
                 className="text-white transition duration-300 hover:text-yellow-200"
+              >
+                Contact
+              </a>
+            </nav>
+          </div>
+        </header>
+      </div>
+      <div className="pt-8">
+        <header className="bg-gradient-to-r from-pink-400 via-purple-500 to-indigo-500">
+          <div className="container mx-auto flex items-center justify-between px-4 py-6">
+            <h1 className="text-3xl font-bold text-white">Header</h1>
+            <nav className="space-x-4">
+              <a
+                href="#"
+                className="text-white transition duration-300 hover:text-pink-200"
+              >
+                Home
+              </a>
+              <a
+                href="#"
+                className="text-white transition duration-300 hover:text-pink-200"
+              >
+                About
+              </a>
+              <a
+                href="#"
+                className="text-white transition duration-300 hover:text-pink-200"
+              >
+                Services
+              </a>
+              <a
+                href="#"
+                className="text-white transition duration-300 hover:text-pink-200"
+              >
+                Contact
+              </a>
+            </nav>
+          </div>
+        </header>
+      </div>
+      <div className="pt-8">
+        <header className="bg-gradient-to-r from-teal-400 via-green-500 to-blue-500">
+          <div className="container mx-auto flex items-center justify-between px-8 py-4">
+            <h1 className="text-4xl font-bold text-white">Header</h1>
+            <nav className="space-x-6">
+              <a
+                href="#"
+                className="text-white transition duration-300 hover:text-yellow-300"
+              >
+                Home
+              </a>
+              <a
+                href="#"
+                className="text-white transition duration-300 hover:text-yellow-300"
+              >
+                About
+              </a>
+              <a
+                href="#"
+                className="text-white transition duration-300 hover:text-yellow-300"
+              >
+                Services
+              </a>
+              <a
+                href="#"
+                className="text-white transition duration-300 hover:text-yellow-300"
               >
                 Contact
               </a>
